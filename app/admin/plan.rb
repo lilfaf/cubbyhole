3e51dd40e00db95039bb5c6a9e72654707443ee6
gmourier <guillaume.mourier.betton@gmail.com>
2014-03-15 17:21:25 +0100

    Add Plan ressource into active-admin

---
+++ app/admin/plan.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register Plan do
+
+
+  # See permitted parameters documentation:
+  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  permit_params :name, :price, :max_storage_space, :max_bandwidth_up, :max_bandwidth_down, :daily_shared_links_quota
+  #
+  # or
+  #
+  # permit_params do
+  #  permitted = [:permitted, :attributes]
+  #  permitted << :other if resource.something?
+  #  permitted
+  # end
+
+end
