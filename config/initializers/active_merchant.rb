0a7471f1386b21453c4d8e849ceb3fefeaf54e6e
gmourier <guillaume.mourier.betton@gmail.com>
2014-03-16 17:46:12 +0100

    active_merchant initializer

---
+++ config/initializers/active_merchant.rb
@@ -0,0 +1,8 @@
+require 'active_merchant'
+
+ActiveMerchant::Billing::Base.mode = :test
+  ::GATEWAY = ActiveMerchant::Billing::PaypalExpressGateway.new(
+  :login => "cubbyholesupersonic-facilitator_api1.gmail.com",
+  :password => "1391005785",
+  :signature => "AFcWxV21C7fd0v3bYYYRCpSSRl31AYy7YtND7y2GrQo5eoDJijXGShHH"
+)
