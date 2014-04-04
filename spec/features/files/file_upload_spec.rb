1963b5cb6e2cf43e5c1f6c0fc6474c32cc59a6dc
Louis Larpin <louis.larpin@gmail.com>
2014-04-04 04:07:06 +0200

    Fix broken tests

--- spec/features/files/file_upload_spec.rb
+++ spec/features/files/file_upload_spec.rb
@@ -8,6 +8,7 @@ feature "Users" do
   scenario "uploading a file" do
     login_as(user)
     visit app_url
-    attach_file("asset_image", "#{Rails.root}/spec/support/fixtures/rails.png")
+    attach_file("file", "#{Rails.root}/spec/support/fixtures/rails.png")
+    # submit requires phantomjs ?
   end
 end
