845dabbc4776e28ddaa626179eb6860079e4617f
Louis Larpin <louis.larpin@gmail.com>
2014-04-04 03:20:41 +0200

    Use foundation helper in devise views

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,35 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Plan.create!(name: 'free',
+             price: 0,
+             max_storage_space: 1024,
+             max_bandwidth_up: 100,
+             max_bandwidth_down: 500,
+             daily_shared_links_quota: 50)
+
+Plan.create!(name: 'silver',
+             price: 10,
+             max_storage_space: 10240,
+             max_bandwidth_up: 300,
+             max_bandwidth_down: 1000,
+             daily_shared_links_quota: 500)
+
+Plan.create!(name: 'gold',
+             price: 15,
+             max_storage_space: 102400,
+             max_bandwidth_up: 1000,
+             max_bandwidth_down: 1000,
+             daily_shared_links_quota: 1000)
+
+Plan.create!(name: 'platinium',
+             price: 20,
+             # 0 = unlimited
+             max_storage_space: 0,
+             max_bandwidth_up: 0,
+             max_bandwidth_down: 0,
+             daily_shared_links_quota: 0)
+
 User.create!(username: 'member',
              email: 'member@example.com',
              password: '12345678',
