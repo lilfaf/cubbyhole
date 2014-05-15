38317360d5e6a3c6da758250c73ebe42d85f418c
Louis Larpin <louis.larpin@gmail.com>
2014-05-15 19:42:34 +0200

    Add links for folders only

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 module ApplicationHelper
   include FoundationRailsHelper::FlashHelper
+
+  def foundation_icon_for(resource)
+    icon = resource.is_a?(Folder) ? 'fi-folder' : 'fi-page'
+    content_tag :i, nil, class: "#{icon} large-icon"
+  end
 end
